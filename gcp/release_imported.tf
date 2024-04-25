1ddfffb0241b579de90f8f263dbf533ad34a3e87
Tim Ramlot <42113979+inteon@users.noreply.github.com>
2024-04-25 17:47:42 +0200

    remove unused infrastructure
    Signed-off-by: Tim Ramlot <42113979+inteon@users.noreply.github.com>

--- gcp/release_imported.tf
+++ gcp/release_imported.tf
@@ -13,60 +13,6 @@ resource "google_storage_bucket" "cert_manager_release_backup_2022_03_30" {
   uniform_bucket_level_access = true
 }
 
-// Firewall rules
-
-resource "google_compute_firewall" "default_allow_icmp" {
-  allow {
-    protocol = "icmp"
-  }
-
-  description   = "Allow ICMP from anywhere"
-  direction     = "INGRESS"
-  name          = "default-allow-icmp"
-  network       = "https://www.googleapis.com/compute/v1/projects/cert-manager-release/global/networks/default"
-  priority      = 65534
-  project       = module.cert-manager-release.project_id
-  source_ranges = ["0.0.0.0/0"]
-}
-
-resource "google_compute_firewall" "default_allow_internal" {
-  allow {
-    ports    = ["0-65535"]
-    protocol = "tcp"
-  }
-
-  allow {
-    ports    = ["0-65535"]
-    protocol = "udp"
-  }
-
-  allow {
-    protocol = "icmp"
-  }
-
-  description   = "Allow internal traffic on the default network"
-  direction     = "INGRESS"
-  name          = "default-allow-internal"
-  network       = "https://www.googleapis.com/compute/v1/projects/cert-manager-release/global/networks/default"
-  priority      = 65534
-  project       = module.cert-manager-release.project_id
-  source_ranges = ["10.128.0.0/9"]
-}
-
-// Service accounts
-
-resource "google_service_account" "dns01_solver" {
-  account_id   = "dns01-solver"
-  display_name = "dns01-solver"
-  project      = module.cert-manager-release.project_id
-}
-
-resource "google_service_account" "give_me_my_cluster" {
-  account_id   = "give-me-my-cluster"
-  display_name = "For the give-me-my-cluster CLI"
-  project      = module.cert-manager-release.project_id
-}
-
 // IAM roles
 
 resource "google_project_iam_custom_role" "cloudkmskeyversiongetter" {
