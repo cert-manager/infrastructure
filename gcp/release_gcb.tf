f67efe28fd6d70a69e0dafce9065b78d904ab8da
Tim Ramlot <42113979+inteon@users.noreply.github.com>
2024-06-04 16:38:58 +0200

    remove cert-manager-package-debian gcb job
    Signed-off-by: Tim Ramlot <42113979+inteon@users.noreply.github.com>

--- gcp/release_gcb.tf
+++ gcp/release_gcb.tf
@@ -17,26 +17,3 @@ resource "google_cloudbuild_trigger" "cert-manager-build-on-tag" {
     }
   }
 }
-
-resource "google_cloudbuild_trigger" "cert-manager-package-debian" {
-  description = "Builds the debian-based trust package for trust-manager. Run regularly via a scheduled trigger"
-
-  location = "global"
-  name     = "cert-manager-package-debian"
-  project  = module.cert-manager-release.project_id
-
-  approval_config {
-    approval_required = false
-  }
-  git_file_source {
-    path      = "gcb/ci-update-debian-trust-package.yaml"
-    repo_type = "GITHUB"
-    revision  = "refs/heads/main"
-    uri       = "https://github.com/cert-manager/trust-manager"
-  }
-  source_to_build {
-    ref       = "refs/heads/main"
-    repo_type = "GITHUB"
-    uri       = "https://github.com/cert-manager/trust-manager"
-  }
-}
